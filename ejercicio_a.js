3726d7f1d8ced5061b2a415610ca3451a9d80d89
diego.soria.galvarro <diego.soria.galvarro@gmail.com>
2024-08-15 19:04:04 -0300

    ejercico a

---
+++ ejercicio_a.js
@@ -0,0 +1,35 @@
+
+
+
+let cantidadDeGatos = parseInt(prompt('Ingrese cantidad de Gatos'));
+
+if ( cantidadDeGatos < 1)  
+    console.log(cantidadDeGatos + ' es una Cantidad DESCONOCIDA');
+    
+
+
+
+
+
+emojis = ['😺','😸','😹']
+var j=0;
+
+
+console.log('Ejercicio a:');
+
+for ( var i=0 ; i<cantidadDeGatos ; ++i){
+    
+    console.log('Gato #'+ (i+1) +':'+ emojis[j]);
+    j++;
+        
+    if (j>2) j=0;
+    
+    
+    
+
+}
+
+
+
+
+
